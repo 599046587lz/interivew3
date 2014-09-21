5af3cfbd266267ebdaa2e6dcfbbded6471027ee6
bangbang93 <bangbang93@163.com>
2014-09-21 19:01:46 +0800

    上传archive判断登录

--- routes/club.js
+++ routes/club.js
@@ -54,7 +54,7 @@ router.get('/logout', r.checkLogin, function (req,res){
  */
 router.post('/upload/archive', function (req, res){
     var file = req.files.archive;
-    if(!file){
+    if(!file || !req.session['cid']){
         return res.send(403);
     } else {
         club.handleArchive(file, req.session['cid'], function (err){

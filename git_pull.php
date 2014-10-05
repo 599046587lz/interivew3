7bd4b0ffef81093d28b98c4fc03698f874767cc2
bangbang93 <bangbang93@163.com>
2014-10-05 16:40:23 +0800

    修改git pull脚本

--- git_pull.php
+++ git_pull.php
@@ -1,2 +1,8 @@
 <?php
-echo exec('git pull');
+$input = fopen('php://input');
+$input = json_decode($input);
+if (!empty($input->pullrequest_merged)){
+  echo exec('git pull');
+} else{
+  echo 'not merged';
+}

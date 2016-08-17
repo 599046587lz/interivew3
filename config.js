441a2d0e943eaa2a93c911e53cf8cabb72af962b
jerry <1332411592@qq.com>
2016-08-17 16:56:51 +0800

    fix : add global config file

---
+++ config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by bangbang93 on 14-9-15.
+ */
+module.exports = {
+    db:{
+        host: 'localhost',
+        db: 'interview'
+    },
+    cookie_secret: 'redhome2014interview'
+};

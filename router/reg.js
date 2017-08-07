45726bcf9af55a16807f15f5174ae60536c48b14
Benmix <wubendao@gmail.com>
2017-08-07 16:55:45 +0800

    change again

--- router/reg.js
+++ router/reg.js
@@ -8,19 +8,19 @@ let router = express.Router();
 let wrap = fn => (...args) => fn(...args).catch(args[2]);
 
 router.post('/', wrap(async function (req, res, next) {
-    let data = req.body;
-    let result;
-    if (!data || !!Object.keys(data).length == 0) throw new Error('数据丢包，请重新输入！');
-    else {
+        let data = req.body;
+        let result;
+        if (!data || !!Object.keys(data).length == 0) throw new Error('数据丢包，请重新输入！');
+        else {
             try {
-                let info ={};
+                let info = {};
 
                 info.name = data.club;
                 info.cid = data.clubID;
                 result = await club.verifyInfo(info);
-                let filename= Date.now()+'.jpg';
+                let filename = Date.now() + '.jpg';
                 let newfilename = 'new' + filename;
-                image_save.image_save(data.pic_url,filename,newfilename);
+                image_save.image_save(data.pic_url, filename, newfilename);
                 data.image = newfilename;
                 result = await student.addStudent(data);
 
@@ -31,25 +31,4 @@ router.post('/', wrap(async function (req, res, next) {
         }
     }
 ));
-//
-// router.post('/', function (req, res) {
-//
-//     let data = req.body;
-//     let send;
-//     (async () => {
-//         try {
-//             let filename= Date.now()+'.jpg';
-//             let newfilename = 'new' + filename;
-//             image_save.image_save(data.pic_url,filename,newfilename);
-//             data.image = newfilename;
-//             send = await student.addStudent(data);
-//
-//             if (!!send) res.send('报名成功！');
-//
-//         } catch (err) {
-//             console.log(err);
-//             res.status(403).send('信息填写有误，请再次确认！');
-//         }
-//     })();
-// });
 module.exports = router;

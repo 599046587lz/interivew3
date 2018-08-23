390cee1b6b12410a7f7b8fe294aff439a684e6e0
DESKTOP-PA5T8FP\宋大帅 <1322023401@qq.com>
2018-08-23 14:12:37 +0800

    修改7牛上传为https

--- public/lib/js/apply.js
+++ public/lib/js/apply.js
@@ -328,7 +328,7 @@ $(function () {
             return;
         }
         $.ajax({
-            url: "http://up-z2.qiniu.com?token=" + data.token,
+            url: "https://up-z2.qiniu.com?token=" + data.token,
             type: "post",
             data: form,
             processData: false,

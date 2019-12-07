5e7e807cbad2b36493b868b80744338a00c944ca
wangxiaoyi <giraffely@icloud.com>
2019-12-07 11:34:28 +0800

    调整login样式

---
+++ public/lib/js/login.js
@@ -0,0 +1,60 @@
+//baseURL = 'http://interview.redhome.cc/';
+//baseURL = 'http://interview.redhome.cc';
+baseURL = '/';
+
+$(function () {
+
+    var pass_field = mdc.textField.MDCTextField.attachTo($("#pass_field")[0])
+    var pass_input = $("#password")[0]
+    var icon = $("#visibility")[0]
+
+    $(".visibility").click(function () {
+        pass_field.foundation_.activateFocus()
+        if(pass_input.type === 'password'){
+            pass_input.type = 'text';
+            icon.style.color = '#3275E0'
+        } else {
+            pass_input.type = 'password';
+            icon.style.color = '#919191'
+        }
+    })
+
+    $(".button").click(function (e) {
+        e.preventDefault()
+
+        var account = $("[name=username]").val();
+        var password = $("[name=password]").val();
+
+        $.ajax({
+            url: baseURL + "common/login",
+            type: 'post',
+            data: JSON.stringify({user: account, password: password}),
+            contentType: "application/json",
+            // dataType: 'json',
+            beforeSend: function () {
+                $(".button").addClass('loading');
+            },
+            statusCode: {
+                200: function () {
+                    success('登陆成功!');
+                    setTimeout(function () {
+                        window.location.href = "select.html";
+                    }, 500);
+                },
+                403: function () {
+                    err("帐号或密码错误！");
+                },
+                404: function () {
+                    err("Page not found!");
+                },
+                500: function () {
+                    err('服务器错误,请重试!');
+                }
+            },
+            complete: function () {
+                $(".button").removeClass('loading');
+            }
+        });
+    });
+});
+

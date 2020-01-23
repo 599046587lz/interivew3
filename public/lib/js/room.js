8f01e4fa19b6b506642fa10f553dc97981ad5205
LZzzno1 <lz599046587@hdu.edu.cn>
2020-01-23 17:12:05 +0800

    --room改动

--- public/lib/js/room.js
+++ public/lib/js/room.js
@@ -1,260 +1,227 @@
-var baseURL = '';
-
-var storage = window.localStorage;
-
-// 排队表格模板
-var waitingTemplate = function (no, sid, name, did, room) {
-    var dids = did.join(',');
-    var rooms = room.join(',');
-    var template = `<tr class = \"template\">
-                        <td>${no}</td>
-                        <td>${sid}</td>
-                        <td>${name}</td>
-                        <td class=\"depart tdOverHide\" >${dids}</td>
-                        <td class=\"tdOverHide\" >${rooms}</td>
-                    </tr>`;
-    return template;
-}
-
-// 呼叫模板
-var callTemplate = function (name, room, did) {
-
-    var template = `<div class = \"template\">
-                        <span class='smallCircle'></span>
-                        <span class='calling'>请${name}同学到${room}教室参加${did}面试</span>
-                    </div>`;
-    return template;
-}
-
-//改变呼叫
-var changeCallRow = function () {
-    $(".current ._default").remove();
-    $(".current .template").remove();
-    var calling = JSON.parse(storage.getItem("calling"));
-    if (calling.length == 0) {
-        var a = "<div class=\"_default\"><span class=\"smallCircle\"></span><span class=\"calling\">暂无呼叫</span></div>";
-        $(".current").append(a);
-    } else {
-        var departmentInfo = JSON.parse(storage.getItem("departmentInfo"));
-        for (var i = 0; i < calling.length; i++) {
-            var room;
-            var did;
-            for (var j = 0; j < departmentInfo[0].departments.length; j++) {
-                if (parseInt(calling[i].calldid) == departmentInfo[0].departments[j].did) {
-                    did = departmentInfo[0].departments[j].name;
-                    room = departmentInfo[0].departments[j].location;
-                }
-            }
-            var a = callTemplate(calling[i].name, room, did);
-            $(".current").append(a);
+    var number = 3;
+    var f1 = true;
+    var f2 = true;
+    var f3 = true;
+    function add()
+    {
+        var a = document.getElementById('room_circle');
+        var b = document.getElementById('room_circle2');
+        a.style.display = "none";
+        b.style.display = "block";
+        b.style.marginTop = "30px";
+    }
+    function add_return()
+    {
+        var a = document.getElementById('room_circle');
+        var b = document.getElementById('room_circle2');
+        var h = document.getElementById("staffid").value;
+        a.style.display = "block";
+        b.style.display = "none";
+        var c = document.createElement("div");
+        c.className = "room_main2";
+        var div2= document.createElement("div");
+        div2.className = "room_border2";
+        c.appendChild(div2);
+        var div3 = document.createElement("div");
+        div3.className = "cirle_number";
+        div2.appendChild(div3);
+        var span1 = document.createElement("span");
+        span1.innerHTML = number;
+        if (number<10)
+            span1.className = "span_circle";
+        else span1.className = "span_circle2";
+        number++;
+        div3.appendChild(span1);
+        var div6 = document.createElement("div");
+        div6.className = "interviewee";
+        var span2 = document.createElement("span");
+        span2.innerHTML = "Jerry";
+        span2.className = "span1";
+        var span3 = document.createElement("span");
+        span3.innerHTML = h;
+        span3.className = "span2";
+        div6.appendChild(span2);
+        div6.appendChild(span3);
+        div2.appendChild(div6);
+        var div4 = document.createElement("div");
+        div4.className = "department1";
+        div4.innerHTML = "设计部";
+        div2.appendChild(div4);
+        var div5 = document.createElement("div");
+        div5.className = "department2";
+        div5.innerHTML = "技术部";
+        div2.appendChild(div5);
+        // var d = document.getElementById("tianjia");
+        //a.parentNode.insertBefore(c, a);
+        var hhh = document.getElementById("dier");
+        hhh.appendChild(c);
+        var butn = document.createElement("button");
+        butn.className = "mdc-button mdc-button--raised submit button btn1";
+        butn.innerHTML = "面试";
+        butn.onclick = function(){
+            up($(this));
+        } 
+        div2.appendChild(butn);
+        // alert("添加成功");
+    }
+    function mohu(obj){
+        var divv = document.createElement("div");
+        divv.className = "room_border_mohu";
+        divv.style.display = "block";
+        var p3 = document.createElement("p");
+        p3.className = "p1";
+        p3.innerHTML = "you need go to";
+        divv.appendChild(p3);
+        var p4 = document.createElement("p");
+        p4.className = "p2";
+        p4.innerHTML = "210 room";
+        divv.appendChild(p4);
+        var divd = document.createElement("div");
+        divd.className = "skip";
+        divd.innerHTML = "skip";
+        divv.appendChild(divd);
+        divd.onclick = function(){
+            var k = confirm("确定取消此人吗");
+            if (k == true)
+                aa.remove();
         }
-        if (calling.length > 10) {
-            $(".callScroll").css({"height": "305px"});
-            $(".callScroll").jScrollPane();
+        var dive = document.createElement("div");
+        dive.className = "ok";
+        dive.innerHTML = "ok";
+        dive.onclick = function(){
+            var k = confirm("确定让此人吗");
+            if (k == true)
+                aa.remove();
+        }
+        divv.appendChild(dive);
+        divv.onclick = function(){
+            divv.remove();
+        }
+        var aa = obj.parentNode;
+        aa.appendChild(divv);
+        // <div class="bull">
+            // <div class="room_border" onclick="mohu1()"  id="clear1" >
+            // <div class="cirle_number"><span class="span_circle">1</span></div>
+            // <span class="span1">Jerry</span>
+            // <div class="department">技术部</div>
+            // </div>
+            // <div class="room_border_mohu"  id="mohu1" onclick="yikai1()">
+            // <p class="p1">you need go to </p> 
+            // <p class="p2">210 room</p> 
+            // <div class="skip" onclick="skip($(this))">skip</div>
+            // <div class="ok" onclick="ok($(this))">ok</div>
+            // </div>
+            // <!-- <div class="kong" style="margin-left: 10px;" id="kong1"></div> -->
+            // </div>
+        }
+        function mohu1()
+        {
+            var a =document.getElementById("mohu1");
+            a.style.display = "block";
+        //var b = document.getElementById("clear1");
+    }
+    function mohu2()
+    {
+        var a =document.getElementById("mohu2");
+        a.style.display = "block";
+        //var b = document.getElementById("clear2");
+    }
+    function mohu3()
+    {
+        var a =document.getElementById("mohu3");
+        a.style.display = "block";
+        //var b = document.getElementById("clear3");
+    }
+    function yikai1()
+    {
+        var a =document.getElementById("mohu1");
+        a.style.display = "none";
+    }
+    function yikai2()
+    {
+        var a =document.getElementById("mohu2");
+        a.style.display = "none";
+    }
+    function yikai3()
+    {
+        var a =document.getElementById("mohu3");
+        a.style.display = "none";
+    }
+    function up(obj)
+    {
+        var k = confirm("确定让此人面试吗");
+        if (k == true){
+            obj.parents(".room_main2").remove();
+            var divz = document.createElement("div");
+            divz.className = "bull";
+
+            var diva = document.createElement("div");
+            diva.className = "room_border";
+            diva.onclick = function()
+            {
+                mohu(this);
+            }
+            divz.appendChild(diva);
+            var divb = document.createElement("div");
+            divb.className = "cirle_number";
+            diva.appendChild(divb);
+            var spana = document.createElement("span");
+            spana.className = "span_circle";
+            spana.innerHTML = "1";
+            divb.appendChild(spana);
+            var spanb = document.createElement("span");
+            spanb.className = "span1";
+            spanb.innerHTML = "Jerry";
+            diva.appendChild(spanb);
+            var divc = document.createElement("div");
+            divc.className = "department";
+            divc.innerHTML = "技术部";
+            diva.appendChild(divc);
+            var jj = document.getElementById("wait");
+            jj.appendChild(divz);
+            // var jj = document.getElementsByClassName("bull_wai");
+            // jj.appendChild(divz);
+            // var div5 = document.createElement("div");
+            // div5.className = "department2";
+            // div5.innerHTML = "技术部";
+            // div2.appendChild(div5);
+
+            // <div class="bull">
+            // <div class="room_border" onclick="mohu1()"  id="clear1" >
+            // <div class="cirle_number"><span class="span_circle">1</span></div>
+            // <span class="span1">Jerry</span>
+            // <div class="department">技术部</div>
+            // </div>
+            // <div class="room_border_mohu"  id="mohu1" onclick="yikai1()">
+            // <p class="p1">you need go to </p> 
+            // <p class="p2">210 room</p> 
+            // <div class="skip" onclick="skip($(this))">skip</div>
+            // <div class="ok" onclick="ok($(this))">ok</div>
+            // </div>
+            // <!-- <div class="kong" style="margin-left: 10px;" id="kong1"></div> -->
+            // </div>
         }
     }
-
-}
-// 改变排队
-var changeWaitRow = function () {
-    var waiting = JSON.parse(storage.getItem("waiting"));
-    var departmentInfo = JSON.parse(storage.getItem("departmentInfo"));
-    var waitingContainer = $('.waitingList tbody');
-    $(".waitingList ._default").remove();
-    $(".waitingList .template").remove();
-
-    for (var i = 0; i < waiting.length; i++) {
-        var did = [];
-        var room = [];
-        for (var j = 0; j < departmentInfo[0].departments.length; j++) {
-            for (var k = 0; k < waiting[i].volunteer.length; k++) {
-                if (waiting[i].volunteer[k] == departmentInfo[0].departments[j].did) {
-                    did[k] = departmentInfo[0].departments[j].name;
-                    room[k] = departmentInfo[0].departments[j].location;
-                }
-            }
-        }
-
-        var a = waitingTemplate(i + 1, waiting[i].sid, waiting[i].name, did, room);
-        waitingContainer.append(a);
+    function ok(obj)
+    {
+        var k = confirm("确定让此人面试吗");
+        if (k == true)
+            obj.parents(".bull").remove();
     }
-    if (waiting.length > 7)
-        $(".waitingList").jScrollPane();
-
-    // 测试
-    // for(var i = 0; i < stuLine.length; i ++){
-    //     var a = waitingTemplate(i + 1, stuLine[i].sid, stuLine[i].name, [0,1], ["7jiao","6jiaoDF VZSVFSDFDS fSDFsdC"]);
-    //     // console.log(a);
-    //     waitingContainer.append(a);
-    // }
-    // if(stuLine.length > 7)
-    //     $(".waitingList").jScrollPane();
-
-    //设置单元格宽度
-    var tdHeader = $(".tHead").find("td");                  //获取表头对象
-    var tds = $('.template').find("td");                    //获取表格对象
-    for (var i = 0; i < tdHeader.length; i++) {
-        $(tds.eq(i)).width(tdHeader.eq(i).width());
+    function skip(obj)
+    {
+        var k = confirm("确定取消此人吗");
+        if (k == true){
+            obj.parents(".bull").remove();
+        }
     }
-
-}
-
-// 面试人数
-var interviewStatus = function () {
-    $(".interviewedNumber").html(storage.getItem("interviewed"))
-    var waiting = JSON.parse(storage.getItem("waiting"));
-    $(".waitingNumber").html(waiting.length);
-}
-
-
-var getDepartmentInfo = function () {
-    $.ajax({
-        url : baseURL + 'room/getDepartmentInfo',
-        type : 'get',
-        dataType : 'json',
-        success: function(data){
-            storage.setItem("departmentInfo", JSON.stringify(data));
-        },
-        statusCode: {
-            403: function () {
-                relogin()
-            },
-            404: function () {
-                err("Page not found!");
-            },
-            500: function () {
-                err('服务器错误,请重试!');
-            }
-        }
-    });
-}
-
-var getFinishNumber = function () {
-    $.ajax({
-        url: baseURL + '/room/finish',
-        type: 'get',
-        dataType: 'json',
-        success: function (data) {
-            storage.setItem("interviewed", data.data);
-            interviewStatus();
-        },
-        statusCode: {
-            500: function () {
-                err('服务器错误,请重试!');
-            }
-        }
-    });
-}
-
-var getCalling = function () {
-    $.ajax({
-        url : baseURL + '/room/calling',
-        type : 'get',
-        dataType : 'json',
-        success:function(data){
-            storage.setItem("calling", JSON.stringify(data.data()));
-            changeCallRow();
-        },
-        statusCode: {
-            404: function () {
-                err("Page not found!");
-            },
-            500: function () {
-                err('服务器错误,请重试!');
-            }
-        }
-    });
-
-}
-
-var getWaiting = function () {
-
-    $.ajax({
-        url: baseURL + '/room/sighed',
-        type: 'get',
-        dataType: 'json',
-        success: function (data) {
-            storage.setItem("waiting", JSON.stringify(data.data));
-            changeWaitRow();
-        },
-        statusCode: {
-            404: function () {
-                err("Page not found!");
-            },
-            500: function () {
-                err('服务器错误,请重试!');
-            },
-            // 200 : function(data){
-            //         storage.setItem("waiting", JSON.stringify(data));
-            //         changeWaitRow();
-            //     },
-        }
-    });
-
-}
-
-// 签 到
-var signin = function () {
-    var input = $("input[ name = sid ]");
-    var stuID = input.val();
-    if (!stuID) {
-        err("请输入学号");
-        input.focus();
-        return false;
+    function step_right()
+    {
+         var hh = document.getElementById("wait").scrollLeft;
+         document.getElementById("wait").scrollLeft = hh + 732;
     }
-    $.ajax({
-        url: baseURL + '/room/sign',
-        type: 'get',
-        data: {
-            sid: stuID
-        },
-        dataType: 'json',
-        success: function (data) {
-            console.log("签到");
-            console.log(data);
-        },
-        statusCode: {
-            404: function () {
-                err("Page not found!");
-            },
-            403: function () {
-                err("该学生未报名!");
-            },
-            204: function () {
-                success("该学生已经签到过了!");
-            },
-            // 待开发的部门选择
-            // 205 : function(){
-            //     selectDepDiv();
-            // },
-            500: function () {
-                err('服务器错误,请重试!');
-            },
-            200: function () {
-                success("签到成功!");
-            }
-        }
-    });
-};
-
-// 展示页面(呼叫,排队,面试状态)
-var toShow = function () {
-    //更新数据
-    getFinishNumber();
-    getWaiting();
-    getCalling();
-}
-
-
-$(function () {
-    $('.back').click(function () {
-        window.history.back();
-    });
-    getDepartmentInfo();
-    toShow();
-
-    $(".signin .submit").click(signin);                     //签到
-
-    setInterval(toShow, 3000);               //刷新展示页面/3s
-
-});
+    function step_left()
+    {
+         var hh = document.getElementById("wait").scrollLeft;
+         document.getElementById("wait").scrollLeft = hh - 732;
+    }

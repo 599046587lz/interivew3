a0e9a0b48bef2502c514136db73c9c2b21ad2952
LZzzno1 <lz599046587@hdu.edu.cn>
2020-02-05 16:15:35 +0800

    jQuery写法改动

--- public/lib/js/room.js
+++ public/lib/js/room.js
@@ -1,36 +1,32 @@
     var number = 2;
-    document.getElementById("left").onclick = function() {stepLeft();}
-    document.getElementById("right").onclick = function() {stepRight();}
-    document.getElementById("addCircle").onclick = function() {add();}
-    document.getElementById("returnMember").onclick = function() {back();}
-    document.getElementById("checkAdd").onclick = function() {addReturn();}
+    var addCircle = $("#addCircle");
+    var submitCircle = $("#submitCircle");
+    var left = $("#left");
+    var right = $("#right");
+    addCircle.click(function(){add();});
+    left.click(function(){stepLeft();});
+    right.click(function(){stepRight();});
+    $("#returnMember").click(function(){back();});
+    $("#checkAdd").click(function(){addReturn();});
     function add()
     {
-        var addCircle = document.getElementById('addCircle');
-        var submitCircle = document.getElementById('submitCircle');
-        addCircle.style.display = "none";
-        submitCircle.style.display = "block";
-        submitCircle.style.marginTop = "30px";
+        addCircle.css('display','none');
+        submitCircle.css('display','block');
     }
     function back()
     {
-        var addCircle = document.getElementById('addCircle');
-        var submitCircle = document.getElementById('submitCircle');
-        addCircle.style.display = "block";
-        submitCircle.style.display = "none";
+        addCircle.css('display','block');
+        submitCircle.css('display','none');
     }
     function addReturn()
     {
-        var addCircle = document.getElementById('addCircle');
-        var submitCircle = document.getElementById('submitCircle');
-        var staffid = document.getElementById("staffid").value;
-        addCircle.style.display = "block";
-        submitCircle.style.display = "none";
+        addCircle.css('display','block');
+        submitCircle.css('display','none');
         var part = `<div class="cover">
         <span>
-        <div class="circleNumber">1</div>
+        <div class="circleNumber">${number}</div>
         <span class="stdName">Jerry</span>
-        <span class="stdNumber">`+staffid+`</span>
+        <span class="stdNumber">${$("#staffid").val()}</span>
         </span>
         <div class="mdc-chip-set">
         <div class="mdc-chip">
@@ -44,6 +40,7 @@
         </div>
         </div>
         </div>`;
+        number++;
         $("#roomContainer").append(part);
         // var butn = document.createElement("button");
         // butn.className = "mdc-button mdc-button--raised submit button btn1";
@@ -64,124 +61,40 @@
             event.stopPropagation();
             this.remove();
         });
+        $(".ok").on("click",function(event){
+            var roomVague = this.parentNode;
+            roomVague.parentNode.remove();
+        });
+        $(".skip").on("click",function(event){
+            var roomVague = this.parentNode;
+            roomVague.parentNode.remove();
+        });
     })
-
-    // function vague(obj){
-    //
-    //     // $(".ok").on("click",function(event){
-    //     //     event.stopPropagation();
-    //     //     var k = confirm("确定让此人面试吗");
-    //     //     if (k == true)
-    //     //         obj.remove();
-    //     // });
-    //     // $(".skip").on("click",function(event){
-    //     //     event.stopPropagation();
-    //     //     var k = confirm("确定取消此人吗");
-    //     //     if (k == true)
-    //     //         obj.remove();
-    //     // });
-    // }
-    // function up(obj)
-    // {
-    //  var k = confirm("确定让此人面试吗");
-    //  if (k == true){
-    //      obj.parents(".room_main2").remove();
-    //      var divz = document.createElement("div");
-    //      divz.className = "bull";
-
-    //      var diva = document.createElement("div");
-    //      diva.className = "room_border";
-    //      diva.onclick = function()
-    //      {
-    //          mohu(this);
-    //      }
-    //      divz.appendChild(diva);
-    //      var divb = document.createElement("div");
-    //      divb.className = "cirle_number";
-    //      diva.appendChild(divb);
-    //      var spana = document.createElement("span");
-    //      spana.className = "span_circle";
-    //      spana.innerHTML = "1";
-    //      divb.appendChild(spana);
-    //      var spanb = document.createElement("span");
-    //      spanb.className = "span1";
-    //      spanb.innerHTML = "Jerry";
-    //      diva.appendChild(spanb);
-    //      var divc = document.createElement("div");
-    //      divc.className = "department";
-    //      divc.innerHTML = "技术部";
-    //      diva.appendChild(divc);
-    //      var jj = document.getElementById("wait");
-    //      jj.appendChild(divz);
-    //      // var jj = document.getElementsByClassName("bull_wai");
-    //      // jj.appendChild(divz);
-    //      // var div5 = document.createElement("div");
-    //      // div5.className = "department2";
-    //      // div5.innerHTML = "技术部";
-    //      // div2.appendChild(div5);
-
-    //      // <div class="bull">
-    //      // <div class="room_border" onclick="mohu1()"  id="clear1" >
-    //      // <div class="cirle_number"><span class="span_circle">1</span></div>
-    //      // <span class="span1">Jerry</span>
-    //      // <div class="department">技术部</div>
-    //      // </div>
-    //      // <div class="room_border_mohu"  id="mohu1" onclick="yikai1()">
-    //      // <p class="p1">you need go to </p>
-    //      // <p class="p2">210 room</p>
-    //      // <div class="skip" onclick="skip($(this))">skip</div>
-    //      // <div class="ok" onclick="ok($(this))">ok</div>
-    //      // </div>
-    //      // <!-- <div class="kong" style="margin-left: 10px;" id="kong1"></div> -->
-    //      // </div>
-    //  }
-    // }
     function stepRight()
     {
-        var scrollLeft = document.getElementById("wait").scrollLeft;
-        // alert(scrollLeft);
-        document.getElementById("wait").scrollLeft = scrollLeft + 680;
+        var scrollLeft = $("#wait").scrollLeft();
+        $("#wait").scrollLeft(scrollLeft + 680);
         scrollLeft += 680;
-        if (scrollLeft <= 0) {
-            var a = document.getElementById("left");
-            a.style.display  = "none";
-        }
+        if (scrollLeft <= 0) 
+            left.css('display','none');
         else
-        {
-            var a = document.getElementById("left");
-            a.style.display  = "block";
-        }
-        if (scrollLeft > document.getElementById("wait").scrollLeft)
-        {
-            var b = document.getElementById("right");
-            b.style.display  = "none";
-        }
-        else{
-            var b = document.getElementById("right");
-            b.style.display  = "block";
-        }
+            left.css('display','block');
+        if (scrollLeft > $("#wait").scrollLeft())
+            right.css('display','none'); 
+        else
+            right.css('display','block'); 
     }
     function stepLeft()
     {
-        var scrollLeft = document.getElementById("wait").scrollLeft;
-        document.getElementById("wait").scrollLeft = scrollLeft - 680;
+        var scrollLeft = $("#wait").scrollLeft();
+        $("#wait").scrollLeft(scrollLeft - 680);
         scrollLeft -= 680;
-        if (scrollLeft <= 0) {
-            var a = document.getElementById("left");
-            a.style.display  = "none";
-        }
+        if (scrollLeft <= 0)
+            left.css('display','none');
         else
-        {
-            var a = document.getElementById("left");
-            a.style.display  = "block";
-        }
-        if (scrollLeft < document.getElementById("wait").scrollLeft && scrollLeft >= 0)
-        {
-            var b = document.getElementById("right");
-            b.style.display  = "none";
-        }
-        else{
-            var b = document.getElementById("right");
-            b.style.display  = "block";
-        }
+            left.css('display','block');
+        if (scrollLeft < $("#wait").scrollLeft() && scrollLeft >= 0)
+            right.css('display','none');
+        else
+            right.css('display','block');
     }

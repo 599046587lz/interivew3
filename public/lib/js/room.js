9c360dcb91de3e1fb710d37115e4dc83608b9afc
wangxiaoyi <wangxiaoyi999@foxmail.com>
2020-02-07 19:45:12 +0800

    feat : 修改签到button

--- public/lib/js/room.js
+++ public/lib/js/room.js
@@ -1,54 +1,43 @@
     var number = 2;
     var addCircle = $("#addCircle");
-    var submitCircle = $("#submitCircle");
     var left = $("#left");
     var right = $("#right");
-    addCircle.click(function(){add();});
-    left.click(function(){stepLeft();});
-    right.click(function(){stepRight();});
-    $("#returnMember").click(function(){back();});
-    $("#checkAdd").click(function(){addReturn();});
-    function add()
-    {
-        addCircle.css('display','none');
-        submitCircle.css('display','block');
-    }
-    function back()
-    {
-        addCircle.css('display','block');
-        submitCircle.css('display','none');
-    }
-    function addReturn()
-    {
-        addCircle.css('display','block');
-        submitCircle.css('display','none');
+    var $staffId = $('#staffId');
+
+
+    addCircle.on('click',function () {
+        addCircle.toggleClass('active');
+    })
+
+    $(".extra").on('click',function (e) {
+        e.stopPropagation()
+    })
+
+    $staffId.on('focus',function () {
+        if ($staffId.val() === 'input staff id'){
+            $staffId.val("")
+        }
+    })
+
+    $("#done").on('click',function () {
+        addCircle.removeClass('active');
         var part = `<div class="cover">
-        <span>
-        <div class="circleNumber">${number}</div>
-        <span class="stdName">Jerry</span>
-        <span class="stdNumber">${$("#staffid").val()}</span>
-        </span>
-        <div class="mdc-chip-set">
-        <div class="mdc-chip">
-        <span class="mdc-chip__text">技术部</span>
-        </div>
-        <div class="mdc-chip" >
-        <span class="mdc-chip__text">媒体运营部</span>
-        </div>
-        <div class="mdc-chip">
-        <span class="mdc-chip__text">媒体运营部</span>
-        </div>
-        </div>
-        </div>`;
+                        <span>
+                            <div class="circleNumber">${number}</div>
+                            <span class="stdName">Jerry</span>
+                            <span class="stdNumber">${$staffId.val()}</span>
+                            </span>
+                            <div class="mdc-chip-set">
+                                 <div class="mdc-chip"><span class="mdc-chip__text">技术部</span></div>
+                                 <div class="mdc-chip" ><span class="mdc-chip__text">媒体运营部</span></div>
+                                 <div class="mdc-chip"><span class="mdc-chip__text">媒体运营部</span></div>
+                            </div>
+                    </div>`;
         number++;
         $("#roomContainer").append(part);
-        // var butn = document.createElement("button");
-        // butn.className = "mdc-button mdc-button--raised submit button btn1";
-        // butn.innerHTML = "面试";
-        // butn.onclick = function(){
-        //     up($(this));
-        // }
-    }
+        $staffId.val("input staff id");
+    })
+
     $(".roomBorder").click(function () {
         var room = `<div class="roomVague">
         <p class="tip">you need to go</p>
@@ -57,32 +46,35 @@
         <div class="ok">ok</div>
         </div>`;
         $(this).append(room);
+        var roomBorder = this;
         $(".roomVague").on("click",function(event){
             event.stopPropagation();
             this.remove();
         });
-        $(".ok").on("click",function(event){
-            var roomVague = this.parentNode;
-            roomVague.parentNode.remove();
+        $(".ok").on("click",function(){
+            roomBorder.remove()
         });
-        $(".skip").on("click",function(event){
-            var roomVague = this.parentNode;
-            roomVague.parentNode.remove();
+        $(".skip").on("click",function(){
+            roomBorder.remove()
         });
     })
+
+    left.click(function(){stepLeft();});
+    right.click(function(){stepRight();});
+
     function stepRight()
     {
         var scrollLeft = $("#wait").scrollLeft();
         $("#wait").scrollLeft(scrollLeft + 680);
         scrollLeft += 680;
-        if (scrollLeft <= 0) 
+        if (scrollLeft <= 0)
             left.css('display','none');
         else
             left.css('display','block');
         if (scrollLeft > $("#wait").scrollLeft())
-            right.css('display','none'); 
+            right.css('display','none');
         else
-            right.css('display','block'); 
+            right.css('display','block');
     }
     function stepLeft()
     {

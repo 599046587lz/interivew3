89040b622c94b89bc1a9d00fdcb3719680a70641
LZzzno1 <lz599046587@hdu.edu.cn>
2020-02-07 23:14:24 +0800

    一些合并错误修改

--- public/lib/js/room.js
+++ public/lib/js/room.js
@@ -5,7 +5,7 @@
     var right = $("#right");
     var $staffId = $('#staffId');
 
-
+   
     addCircle.on('click',function () {
         addCircle.toggleClass('active');
     })
@@ -19,49 +19,30 @@
             $staffId.val("")
         }
     })
-  
-    left.click(function(){stepLeft();});
-    right.click(function(){stepRight();});
 
 
-    $("#wait").on('scroll',function(e){
-        if ($(this).scrollLeft() === 0) {
-            alert('到起点了');
-            left.css("opacity","0");
-        }
-        if ($(this).scrollLeft() > $('.roomBorder').width()*11) {
-            alert('到最右侧了');
-            right.css("opacity","0");
 
-        }
-    })
-    left.click(function(){stepLeft();});
-    right.click(function(){stepRight();});
     $("#done").on('click',function () {
         addCircle.removeClass('active');
+        var name = "好好学习天天向上";
+        if (name.length > 4)
+            name = name.substring(0,4) + "...";
         var part = `<div class="cover">
                         <span>
                             <div class="circleNumber">${number}</div>
-                            <span class="stdName">Jerry</span>
+                            <span class="stdName">${name}</span>
                             <span class="stdNumber">${$staffId.val()}</span>
-                            </span>
-                            <div class="mdc-chip-set">
-                                 <div class="mdc-chip"><span class="mdc-chip__text">技术部</span></div>
-                                 <div class="mdc-chip" ><span class="mdc-chip__text">媒体运营部</span></div>
-                                 <div class="mdc-chip"><span class="mdc-chip__text">媒体运营部</span></div>
-                            </div>
+                        </span>
+                        <div class="mdc-chip-set">
+                            <div class="mdc-chip"><span class="mdc-chip__text">技术部</span></div>
+                            <div class="mdc-chip" ><span class="mdc-chip__text">媒体运营部</span></div>
+                            <div class="mdc-chip"><span class="mdc-chip__text">媒体运营部</span></div>
+                        </div>
                     </div>`;
-   
-
         number++;
         $("#roomContainer").append(part);
-        // var butn = document.createElement("button");
-        // butn.className = "mdc-button mdc-button--raised submit button btn1";
-        // butn.innerHTML = "面试";
-        // butn.onclick = function(){
-        //     up($(this));
-        // }
-    }
+        $staffId.val("input staff id");
+    })
     $(".roomBorder").click(function () {
         var room = `<div class="roomVague">
         <p class="tip">you need to go</p>
@@ -83,12 +64,25 @@
             roomVague.parentNode.remove();
         });
     })
+    left.click(function(){stepLeft();});
+    right.click(function(){stepRight();});
+
+
+    $("#wait").on('scroll',function(e){
+        if ($(this).scrollLeft() === 0) {
+            alert('到起点了');
+            left.css("opacity","0");
+        }
+        if ($(this).scrollLeft() > $('.roomBorder').width()*11) {
+            alert('到最右侧了');
+            right.css("opacity","0");
+
+        }
+    })
     function stepRight()
     {
         var scrollLeft = $("#wait").scrollLeft();
         $("#wait").scrollLeft(scrollLeft + 680);
-        scrollLeft += 680;
-
         left.css("opacity","1");
     }
     function stepLeft()

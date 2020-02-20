1a71b0222daed539a567a5520bb41d0e47cfcdd1
wangxiaoyi <wangxiaoyi999@foxmail.com>
2020-02-20 22:15:50 +0800

    feat : 添加整体的back按钮&调整snackbar

--- public/js/common.js
+++ public/js/common.js
@@ -1,40 +1,65 @@
-function addSnackbar() {
-    var snackbarHtml =
-        `<div class="mdc-snackbar">
+var $back = $('.topBar #back')
+
+$back.on('click',function () {
+  window.history.back();
+})
+
+function addSnackbar(className,content) {
+  var $snackbars = $('.mdc-snackbar')
+  var bottom = $('.mdc-snackbar').length * 60 + 'px'
+  var id = Math.random().toString(36).substr(2);
+  var snackbarHtml =
+    `<div class="mdc-snackbar ${className}" id="${id}">
             <div class="mdc-snackbar__surface">
                 <i class="material-icons sign done">done_all</i>
                 <i class="material-icons sign error">error_outline</i>
-                <div class="mdc-snackbar__label"role="status"aria-live="polite">
+                <div class="mdc-snackbar__label" role="status" aria-live="polite">
+                    ${content}
                 </div>
                 <div class="mdc-snackbar__actions">
                     <button type="button" class="mdc-button mdc-snackbar__action">
-                    <i class="material-icons">clear</i>
+                    <i class="material-icons" id="snackCancel">clear</i>
+                    </button>
+                     <button type="button" class="mdc-button mdc-snackbar__action">
+                    <i class="material-icons" id="snackOk">done</i>
                     </button>
                 </div>
             </div>
         </div>`
-    $('body').append(snackbarHtml)
+  $('body').append(snackbarHtml);
+  var snackbar = mdc.snackbar.MDCSnackbar.attachTo(document.querySelector(`#${id}`))
+  snackbar.root_.addEventListener('MDCSnackbar:closed',function (e) {
+    e.target.remove()
+  });
+  snackbar.root_.style.bottom = bottom;
+  snackbar.open();
+  return id
 }
 
-addSnackbar()
-const snackbar = mdc.snackbar.MDCSnackbar.attachTo(document.querySelector('.mdc-snackbar'))
-
 function success(content) {
-    snackbar.root_.classList.add('success')
-    snackbar.labelText = content;
-    snackbar.open()
+  addSnackbar('success', content)
 }
 
 function err(content) {
-    snackbar.root_.classList.add('error')
-    snackbar.labelText = content;
-    snackbar.open()
+  addSnackbar('error',content)
+}
+
+function snackConfirm(content,cancel,ok) {
+  var id = addSnackbar('confirm',content)
+  var $snackbar = $(`#${id}`);
+  $snackbar.find('#snackCancel').on('click',function () {
+    cancel()
+  })
+  $snackbar.find('#snackOk').on('click',function () {
+    ok()
+  })
+}
+
+var relogin = function () {
+  err("未登录或登录超时")
+  setTimeout(function () {
+    window.location.href = "login.html";
+  }, 500);
 }
 
 
-var relogin = function (){
-    err("未登录或登录超时")
-    setTimeout(function(){
-        window.location.href = "login.html";
-    }, 500);
-}
--- public/js/interview.js
+++ public/js/interview.js
@@ -7,35 +7,36 @@ function StepCtl() {
   this.stepNow = 0
   this.steps = []
 }
-StepCtl.prototype.next = function(number) {
+
+StepCtl.prototype.next = function (number) {
   this.steps[this.stepNow].next(number)
-  this.stepNow ++
+  this.stepNow++
 }
 StepCtl.prototype.push = function (step) {
   this.steps.push(step)
 }
-StepCtl.prototype.pre = function (number) {
+StepCtl.prototype.pre = function () {
   this.steps[this.stepNow].pre()
-  this.stepNow = this.stepNow - number || 1
+  this.stepNow--
 }
 StepCtl.prototype.reStart = function () {
-  this.stepNow = 0;
+  this.stepNow = 1;
 }
 
 function addZero(num) {
   return num < 10 ? '0' + num : num;
 }
 
-function Clock(callback,intervalTime) {
+function Clock(callback, intervalTime) {
   this.min = 0;
   this.sec = 0;
   this.time = "00:00"
   // TODO init Clock
-  this.callback = function() {
+  this.callback = function () {
     this.timeInterval = window.setInterval(function () {
       this.changeTime()
       callback(this.time)
-    }.bind(this),intervalTime || 1000)
+    }.bind(this), intervalTime || 1000)
   }
 }
 
@@ -48,11 +49,11 @@ Clock.prototype.clear = function () {
 }
 
 Clock.prototype.changeTime = function () {
-  if(this.sec === 59){
+  if (this.sec === 59) {
     this.sec = 0;
-    this.min ++;
+    this.min++;
   } else {
-    this.sec ++;
+    this.sec++;
   }
   this.time = addZero(this.min) + ":" + addZero(this.sec);
 }
@@ -63,7 +64,6 @@ Clock.prototype.start = function () {
 // TODO Clock start
 
 
-
 $(function () {
   var $loginCard = $('.login')
   var $callCard = $('.call')
@@ -72,16 +72,14 @@ $(function () {
   var $informationCard = $('.information')
   var $commentCard = $('.comment')
   var $timer = $('#timer');
-  var clock = new Clock(function (time){
+  var clock = new Clock(function (time) {
     $timer[0].innerText = time;
   });
 
   var departmentInfo = null;
   var departments = []
   var $mdcSelect = $('.mdc-select__menu .mdc-list')
-
   var $waitNum = $('.tip .waitNum')
-  // var sid = 0;
 
   var stepCtl = new StepCtl()
   //login to call
@@ -92,13 +90,19 @@ $(function () {
   }))
   //call or call by StaffId & waiting loading
   stepCtl.push(new Step(function () {
-    backtoCall()
+    $callCard.find('.waiting').fadeOut()
+    $callCard.find('.operation').fadeIn()
   }, function (number) {
-    if(number === 0){
-      callNext();
-      $callCard.find('.operation').fadeOut(400, function () {
-        $callCard.find('.waiting').fadeIn(400)
-      })
+    if (number === 0) {
+      if ($waitNum[0].innerText === '0') {
+        err('暂时无人面试')
+        stepCtl.pre()
+      } else {
+        callNext();
+        $callCard.find('.operation').fadeOut(400, function () {
+          $callCard.find('.waiting').fadeIn(400)
+        })
+      }
     } else {
       getStaffId()
     }
@@ -115,8 +119,7 @@ $(function () {
   }))
   //start or skip
   stepCtl.push(new Step(function () {
-    skip()
-    backtoCall()
+    getQueueNumber()
     $commentStep.fadeOut(400, function () {
       $prepareStep.fadeIn()
     })
@@ -125,11 +128,16 @@ $(function () {
     clock.start();
   }))
   //submit
-  stepCtl.push(new Step(null,function () {
+  stepCtl.push(new Step(function () {
+    $commentCard.removeClass('active').addClass('inactive')
+    stepCtl.pre()
+  }, function () {
     submitComment();
     getQueueNumber();
-    backtoCall()
-    $commentStep.fadeOut(400,function () {
+    $callCard.find('.waiting').fadeOut()
+    $callCard.find('.operation').fadeIn()
+    clock.clear()
+    $commentStep.fadeOut(400, function () {
       $prepareStep.fadeIn()
     })
   }))
@@ -145,7 +153,7 @@ $(function () {
     stepCtl.next(0)
   })
   //call by staffId number 1
-  $callCard.find('.link').on('click',function () {
+  $callCard.find('.link').on('click', function () {
     stepCtl.next(1)
   })
 
@@ -153,33 +161,32 @@ $(function () {
   $informationCard.find('button.start').on('click', function () {
     stepCtl.next()
   })
-  $informationCard.find('button.skip').on('click',function () {
-    stepCtl.pre()
+  $informationCard.find('button.skip').on('click', function () {
+    snackConfirm('确认跳过？',null,function () {
+      stepCtl.pre()
+      stepCtl.pre()
+      skip()
+    })
   })
 
   //submit comment
-  $commentCard.find('button.submit').on('click',function () {
+  $commentCard.find('button.submit').on('click', function () {
     stepCtl.next()
     stepCtl.reStart()
   })
 
-  var backtoCall = function () {
-    $callCard.find('.waiting').fadeOut()
-    $callCard.find('.operation').fadeIn()
-  }
-
   //获取社团信息
   var getDepartmentInfo = function () {
     $.ajax({
-      url:'/club/clubInfo',
-      type:'get',
-      statusCode:{
-        200 : function (data) {
+      url: '/club/clubInfo',
+      type: 'get',
+      statusCode: {
+        200: function (data) {
           departmentInfo = data;
           var departmentsHtml = '';
           departmentInfo.departments.forEach(item => {
             departmentsHtml = departmentsHtml.concat(
-                `<li class="mdc-list-item" data-value="${item.name}">${item.name}</li>`)
+              `<li class="mdc-list-item" data-value="${item.name}">${item.name}</li>`)
             departments[item.did] = item.name
           })
           $mdcSelect[0].innerHTML += departmentsHtml;
@@ -192,27 +199,28 @@ $(function () {
     var did = departments.indexOf(select.value);
     var interviewerName = $loginCard.find(' .mdc-text-field input')[0].value;
     $.ajax({
-      url:'/club/setIdentify',
-      type:'post',
-      data:{
-        did:Number(did),
-        interviewerName:interviewerName
+      url: '/club/setIdentify',
+      type: 'post',
+      data: {
+        did: Number(did),
+        interviewerName: interviewerName
       },
-      statusCode : {
-        204 : function(){
+      statusCode: {
+        204: function () {
           getQueueNumber(did);
-        }}
+        }
+      }
     })
   }
 
 
   //获取排队人数
-  var getQueueNumber = function(){
+  var getQueueNumber = function () {
     $.ajax({
-      url:'/interview/queue',
-      type:'get',
-      statusCode:{
-        200 : function (data) {
+      url: '/interview/queue',
+      type: 'get',
+      statusCode: {
+        200: function (data) {
           $waitNum[0].innerText = data
         }
       }
@@ -223,106 +231,111 @@ $(function () {
   var getStaffId = function () {
     var $dialog = $('.mdc-dialog')
     dialog.open();
-    $dialog.find('.cancel').on('click',function () {
+    $dialog.find('.cancel').on('click', function () {
       dialog.close()
       stepCtl.pre();
     })
-    $dialog.find('.ok').on('click',function () {
+    $dialog.find('.ok').on('click', function () {
       var sid = $dialog.find('input')[0].value;
-      dialog.close()
-      $callCard.find('.operation').fadeOut()
-      $callCard.find('.waiting').fadeIn()
-      callNext(sid)
+      if((/[0-9]{8}/).test(sid)){
+        dialog.close()
+        callNext(sid)
+      } else {
+        err('学号格式有误！请重新输入')
+        stepCtl.pre()
+      }
     })
   }
 
   //call or call by staffId
   var callNext = function (sid) {
     var obj = {};
-    if(sid) {
-      obj = {
-        sid:sid
-      }
+    if (sid) {
+      obj = {sid: sid}
     }
-
     $.ajax({
-      url:'/interview/call',
-      type:'get',
-      data:obj,
-      dataType:'json',
-      statusCode:{
-        200 : function () {
-          waitConfirm();
+      url: '/interview/call',
+      type: 'get',
+      data: obj,
+      dataType: 'json',
+      statusCode: {
+        200: function (data) {
+          renderComment(data.name, data.major, data.sid, data.notion)
           stepCtl.next()
+          // waitConfirm();
         },
-        403 : function () {
+        403: function () {
           err('该同学未报名');
+          stepCtl.pre()
           stepCtl.pre()
         }
       }
     })
+
   };
 
-  var waitConfirm = function () {
-    $.ajax({
-      url:'/interview/start',
-      type:'get',
-      statusCode:{
-        200 : function (data) {
-          renderComment(data.name,data.major,data.sid,data.notion)
-          sid = data.sid;
-          stepCtl.next()
-        },
-        403 : function () {
-          alert('此人未确认，已被room跳过');
-          stepCtl.pre(2);
-        },
-        202 : function () {
-          waitConfirm()
-        }
-      }
-    })
-  }
+  // var waitConfirm = function () {
+  //   $.ajax({
+  //     url: '/interview/start',
+  //     type: 'get',
+  //     statusCode: {
+  //       200: function (data) {
+  //         renderComment(data.name, data.major, data.sid, data.notion)
+  //         sid = data.sid;
+  //         stepCtl.next()
+  //       },
+  //       403: function () {
+  //         err('此人未确认，已被room跳过');
+  //         stepCtl.pre();
+  //         stepCtl.pre()
+  //       },
+  //       202: function () {
+  //         waitConfirm()
+  //       }
+  //     }
+  //   })
+  // }
 
-  var renderComment = function (name,specialty,sid,introduction) {
+  var renderComment = function (name, specialty, sid, introduction) {
     $informationCard.find('.name')[0].innerText = name || '--';
     $informationCard.find('.specialty')[0].innerText = specialty || '--';
     $informationCard.find('.tags .mdc-chip__text')[0].innerText = sid || '--';
     $informationCard.find('.introduction')[0].innerText = introduction || '--';
   }
 
-  var submitComment = function(){
+  var submitComment = function () {
     var score = slider.value;
     var comment = $commentCard.find('.note textarea')[0].value
     var sid = $informationCard.find('.mdc-chip .mdc-chip__text')[0].innerText;
 
     $.ajax({
-      url:'/interview/rate',
-      type:'post',
-      data:{
+      url: '/interview/rate',
+      type: 'post',
+      data: {
         sid: sid,
         score: score,
         comment: comment
       },
-      statusCode:{
-        204 : function () {
+      statusCode: {
+        204: function () {
           success('评论成功，将跳转至叫号页面');
         }
       }
     })
   }
 
-  var skip = function(){
+  var skip = function () {
     var sid = $informationCard.find('.mdc-chip .mdc-chip__text')[0].innerText;
     $.ajax({
-      url:'/interview/skip',
-      type:'post',
-      data:{
-        sid:sid,
+      url: '/interview/skip',
+      type: 'post',
+      data: {
+        sid: sid,
       }
     })
   }
 
   getDepartmentInfo()
-  addSnackbar()
+  success('test')
+  err('test2')
 })

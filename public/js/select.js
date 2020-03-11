bd9b57d0d6f83052daa040d54a8c5974b24146ac
wangxiaoyi <wangxiaoyi999@foxmail.com>
2020-03-11 21:54:25 +0800

    feat : 修select的baseUrl

--- public/js/select.js
+++ public/js/select.js
@@ -1,83 +1,83 @@
-baseURL = '';
-var club;
-var set_club = function(){
-	club = {};
-	$.ajax({
-		url : baseURL + '/club/clubInfo',
-		type : 'get',
-		statusCode : {
-			200 : function(data){
-				console.log(data);
-				club = data;
-				set_department();
-			},
-			403 : function(){
-				relogin();
-			},
-			404 : function(){
-				err("Page not found!");
-			},
-			500 : function(){
-				err('服务器错误,请刷新页面再试!');
-			}
-		}
-	});
-};
-
-var set_department = function (){
-	var output      = "";
-	var template    = "<option value='DID'>NAME</option>\n";
-	var dep = club.departments;
-	for( var i in dep){
-		if (dep[i]){
-			var out = template;
-			out = out.replace(/DID/g, dep[i].did);
-			out = out.replace(/NAME/g, dep[i].name);
-			output += out;
-		}
-	}
-	$("#_select").html(output);
-};
-
-
-var setInterviewer = function(){
-	var did = $("#_select").val();
-	var interviewer = $("input[name=interviewer]").val();
-	if ( !interviewer ){
-		err("请输入姓名");
-		return false;
-	}
-	$.ajax({
-		url  : baseURL + "/club/setIdentify",
-		type : 'post',
-		contentType: 'application/json',
-		data : JSON.stringify({did: did, interviewerName: interviewer}),
-		dataType : 'json',
-		statusCode : {
-			404 : function(){
-				err("Page not found!");
-			},
-			403 : function(){
-				err("未登录或登录超时!");
-				relogin();
-			},
-			204 : function(){
-				success('设置成功,即将跳转!');
-				setTimeout(function(){
-					var _did = $("#_select").val();
-					var name = $("#_select [value=" + _did + "]").text();
-					window.location.href = "interview.html#" + encodeURIComponent(name) + '-' + encodeURIComponent(interviewer);
-				}, 500);
-			},
-			500 : function(){
-				err('服务器错误,请重试!');
-			}
-		}
-	});
-};
+baseURL = '/';
 
 $(function(){
-	set_club();
+	var club;
+	var setClub = function(){
+		club = {};
+		$.ajax({
+			url : baseURL + 'club/clubInfo',
+			type : 'get',
+			statusCode : {
+				200 : function(data){
+					console.log(data);
+					club = data;
+					setDepartment();
+				},
+				403 : function(){
+					relogin();
+				},
+				404 : function(){
+					err("Page not found!");
+				},
+				500 : function(){
+					err('服务器错误,请刷新页面再试!');
+				}
+			}
+		});
+	};
+
+	var setDepartment = function (){
+		var output      = "";
+		var template    = "<option value='DID'>NAME</option>\n";
+		var dep = club.departments;
+		for( var i in dep){
+			if (dep[i]){
+				var out = template;
+				out = out.replace(/DID/g, dep[i].did);
+				out = out.replace(/NAME/g, dep[i].name);
+				output += out;
+			}
+		}
+		$("#_select").html(output);
+	};
+
+	var setInterviewer = function(){
+		var did = $("#_select").val();
+		var interviewer = $("input[name=interviewer]").val();
+		if ( !interviewer ){
+			err("请输入姓名");
+			return false;
+		}
+		$.ajax({
+			url  : baseURL + "club/setIdentify",
+			type : 'post',
+			contentType: 'application/json',
+			data : JSON.stringify({did: did, interviewerName: interviewer}),
+			dataType : 'json',
+			statusCode : {
+				404 : function(){
+					err("Page not found!");
+				},
+				403 : function(){
+					err("未登录或登录超时!");
+					relogin();
+				},
+				204 : function(){
+					snackbar.success('设置成功,即将跳转!');
+					setTimeout(function(){
+						var _did = $("#_select").val();
+						var name = $("#_select [value=" + _did + "]").text();
+						window.location.href = "interview.html#" + encodeURIComponent(name) + '-' + encodeURIComponent(interviewer);
+					}, 500);
+				},
+				500 : function(){
+					err('服务器错误,请重试!');
+				}
+			}
+		});
+	};
+
+	setClub();
 	$(".interview").click(function(){
 		$('.dimmer').fadeIn();
 	});
